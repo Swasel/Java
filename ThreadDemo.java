24173d201f6dff8b921b65b61de6e1b68a0ece95
Swathy <swathyselva2205@gmail.com>
2026-06-22 09:59:26 +0530

    Threads

---
+++ ThreadDemo.java
@@ -0,0 +1,44 @@
+//class A extends Thread {
+class A implements Runnable {
+  public void run() {
+    for (int i = 0; i < 5; i++) {
+      System.out.println("Hi");
+      try {
+        Thread.sleep(500);
+      } catch (Exception e) {
+        e.printStackTrace();
+      }
+    }
+  }
+}
+
+// class B extends Thread {
+class B implements Runnable {
+  public void run() {
+    for (int i = 0; i < 5; i++) {
+      System.out.println("Hello");
+      try {
+        Thread.sleep(500);
+      } catch (Exception e) {
+        e.printStackTrace();
+      }
+    }
+  }
+}
+
+public class ThreadDemo {
+  public static void main(String[] args) {
+    // A obj1 = new A();
+    // B obj2 = new B();
+    // obj1.show();
+    // obj2.show();
+    // obj1.start(); // start method to run simultaneously
+    // obj2.start(); // start method present only when Thread is extened
+    Runnable obj1 = new A();
+    Runnable obj2 = new B();
+    Thread t1 = new Thread(obj1);
+    Thread t2 = new Thread(obj2);
+    t1.start();
+    t2.start();
+  }
+}
